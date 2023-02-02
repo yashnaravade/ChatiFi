0ac3b5df938b142820a436cdca4db2f44425058d
Yash Naravade <yashdip123@gmail.com>
2023-02-02 20:55:45 +0530

    feat: Add backend url oof vercel to proxy for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/message": {
+//         target: "http://localhost:5000",
+//         changeOrigin: true,
+//         secure: false,
+//         rewrite: (path) => path.replace(/^\/api/, ""),
+//       },
+//     },
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/message": {
-        target: "http://localhost:5000",
+        target: "https://chati-fi.vercel.app",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
