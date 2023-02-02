bc0b670cd9173523f754085d0d88cfbdbdd331e2
Yash Naravade <yashdip123@gmail.com>
2023-02-02 18:50:06 +0530

    add vercel.json for backend deployment

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const  messageModel = require('./module/message');
+const express = require("express");
+const mongoose = require("mongoose");
+const messageModel = require("./module/message");
 
 const PORT = 5000;
 
@@ -8,42 +8,43 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://vaibhavi:vaibhavi@learnmongodb.eluyb.mongodb.net/?retryWrites=true&w=majority', ()=>{
-    console.log('connected to mongodb');
+mongoose.connect(
+  "mongodb+srv://vaibhavi:vaibhavi@learnmongodb.eluyb.mongodb.net/?retryWrites=true&w=majority",
+  () => {
+    console.log("connected to mongodb");
+  }
+);
+
+app.get("/health", (req, res) => {
+  res.json({
+    success: true,
+  });
 });
 
-app.get('/health', (req, res) => {
-    res.json({
-      success: true,
-    });
-})
-
-app.post('/message',async(req,res) => {
+app.post("/message", async (req, res) => {
   // const user = req.body.user;
   // const messageType = req.body.messageType;
   // const messageBody = req.body.messageBody;
 
-  const {user, messageType, messageBody } = req.body
+  const { user, messageType, messageBody } = req.body;
 
   const newMessage = new messageModel({
-    user : user,
-    messageType : messageType,
-    messageBody : messageBody
-  })
+    user: user,
+    messageType: messageType,
+    messageBody: messageBody,
+  });
 
-  const savedMessage = await  newMessage.save();
+  const savedMessage = await newMessage.save();
 
-  res.json({savedMessage});
-})
+  res.json({ savedMessage });
+});
 
-app.get('/message',async(req,res) => {
+app.get("/message", async (req, res) => {
+  const messages = await messageModel.find();
 
-    const messages =await messageModel.find();
+  res.json(messages);
+});
 
-    res.json(messages);
-    
-} )
-
-app.listen(PORT , () => {
-console.log(`Server is listening on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
